c2775e423e0cefd8eebdc0515b891c6b8a577bdf
sumit kumar <sumit.kumar@colortokens.com>
2019-01-03 21:09:13 +0530

    tree traversal recurse and iterative.

--- trees/bst.py
+++ trees/bst.py
@@ -33,13 +33,13 @@ class BinarySearchTree:
     def preOrderRecurse(self, root):
         if root:
             print(root.data)
-            self.inOrderRecurse(root.left)
-            self.inOrderRecurse(root.right)
+            self.preOrderRecurse(root.left)
+            self.preOrderRecurse(root.right)
 
     def postOrderRecurse(self, root):
         if root:
-            self.inOrderRecurse(root.left)
-            self.inOrderRecurse(root.right)
+            self.postOrderRecurse(root.left)
+            self.postOrderRecurse(root.right)
             print(root.data)
 
     def inOrderIterate(self, root):
@@ -55,14 +55,16 @@ class BinarySearchTree:
                 r = n.right
 
     def preOrderIterate(self, root):
-        s = [root]
-        while len(s):
-            n = s.pop()
-            print(n.data)
-            if n.left:
-                s.append(n.left)
-            if n.right:
-                s.append(n.left)
+        s = []
+        r = root
+        while r or len(s):
+            if r:
+                print(r.data)
+                if(r.right):
+                    s.append(r.right)
+                r = r.left
+            else:
+                r = s.pop()
 
     def postOrderIterate(self, root):
         r = root
@@ -72,18 +74,30 @@ class BinarySearchTree:
                 s.append(r)
                 r = r.left
             else:
-                n = s.pop()
-                print(n.data)
-                r = n.right
+                n = s[-1].right
+                if n:
+                    r = n
+                else:
+                    n = s.pop()
+                    print(n.data)
+                    while len(s) and s[-1].right == n:
+                        n = s.pop()
+                        print(n.data)
+
+    def _ifRightChild(self, node, stack):
+        s = stack[-1]
+        res = True
+        if s.right:
+            res = s.right.data == node.data
+        return res
 
 
 t = BinarySearchTree()
 t.insert(t.root, 10)
-t.insert(t.root, 1)
+t.insert(t.root, 8)
 t.insert(t.root, 110)
 t.insert(t.root, 8)
 t.insert(t.root, 4)
 t.insert(t.root, 11)
 t.insert(t.root, 7)
-t.preOrderIterate(t.root)
-t.preOrderRecurse(t.root)
+
